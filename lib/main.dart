680f8beb6e48ed590660fac513b3de4c40f3e5f2
Jahid01234 <mdjahidhasan790523@gmail.com>
2026-06-04 19:06:10 +0600

    add firebase push notification setup....

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:deep_linking/app.dart';
+import 'package:deep_linking/core/service_class/push_notification_service/push_notification_service.dart';
 import 'package:deep_linking/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
+
+  await FirebaseNotificationService.instance.initialize();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initializationSettings =
+  InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(settings: initializationSettings);
+
   runApp(const MyApp());
 }
 
